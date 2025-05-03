f6c41d3a8189a5f314439565eee1f8640a832f2e
Altivion Group <altiviongroup@gmail.com>
2025-05-03 01:12:14 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -68,5 +68,16 @@ filterType.addEventListener("change", loadLeads);
 // Initial load
 loadLeads();
 
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+
+// Logout functionality
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  const auth = getAuth();
+  signOut(auth).then(() => {
+    window.location.href = "login.html";
+  }).catch((error) => {
+    alert("Error logging out: " + error.message);
+  });
+});
 
 
